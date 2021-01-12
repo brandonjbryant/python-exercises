4222598a26eb978c06596270fbbf3c47305b9234
“Brandon <brandon.j.bryant1@gmail.com>
2021-01-12 10:27:55 -0600

    Third solution completed

--- data_types_and_variables.py
+++ data_types_and_variables.py
@@ -15,4 +15,9 @@ facebook_pay = 350
 total_pay = (google_pay * 6) + (facebook_pay * 10) + (amazon_pay * 4)
 print(total_pay)
 
+### A student can be enrolled to a class only if the class is not full and the class schedule does not conflict with her current schedule. /* 
+is_class_full = True
+no_schedule_conflict = True
+can_be_enrolled = is_class_full and no_schedule_conflict
+print(can_be_enrolled)
 
