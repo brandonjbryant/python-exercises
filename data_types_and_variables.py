fec6c78e9ee3a5defcf4349b366f8a91bd54056a
“Brandon <brandon.j.bryant1@gmail.com>
2021-01-12 10:08:42 -0600

    First variable complete

---
+++ data_types_and_variables.py
@@ -0,0 +1,7 @@
+little_mermaid_rental = 3
+brother_bear_rental = 5
+hercules_rental = 1
+price_per_day = 3
+rental_days= (little_mermaid_rental + brother_bear_rental + hercules_rental)
+total_price = rental_days * price_per_day
+print(total_price)
