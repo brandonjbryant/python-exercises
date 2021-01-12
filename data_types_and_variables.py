278986098021b2636a4f85bb636fb461dbce40ca
“Brandon <brandon.j.bryant1@gmail.com>
2021-01-12 10:20:17 -0600

    Second solution completed

--- data_types_and_variables.py
+++ data_types_and_variables.py
@@ -5,3 +5,14 @@ price_per_day = 3
 rental_days= (little_mermaid_rental + brother_bear_rental + hercules_rental)
 total_price = rental_days * price_per_day
 print(total_price)
+#27 dollars#
+
+
+#2
+google_pay = 400
+amazon_pay = 380
+facebook_pay = 350
+total_pay = (google_pay * 6) + (facebook_pay * 10) + (amazon_pay * 4)
+print(total_pay)
+
+
