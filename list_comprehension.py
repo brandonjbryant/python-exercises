aa7d2b18c2e252a7b1a762d8b248aaab022b1d6e
“Brandon <brandon.j.bryant1@gmail.com>
2021-01-12 14:46:15 -0600

    8/17 Complete

--- list_comprehension.py
+++ list_comprehension.py
@@ -30,18 +30,42 @@ fruits = ['mango', 'kiwi', 'strawberry', 'guava', 'pineapple', 'mandarin orange'
 capitalized_fruits = [fruit.capitalize() for fruit in fruits]
 print(capitalized_fruits)
 # Exercise 3 - Use a list comprehension to make a variable named fruits_with_more_than_two_vowels. Hint: You'll need a way to check if something is a vowel.
+fruits = ['mango', 'kiwi', 'strawberry', 'guava', 'pineapple', 'mandarin orange']
 
+fruits_with_more_than_two_vowels = [fruit for fruit in fruits if (
+    fruit.count("a") + 
+    fruit.count("e") + 
+    fruit.count("i") + 
+    fruit.count("o") + 
+     fruit.count("u")) > 2]
+print(fruits_with_more_than_two_vowels)
 # Exercise 4 - make a variable named fruits_with_only_two_vowels. The result should be ['mango', 'kiwi', 'strawberry']
+fruits = ['mango', 'kiwi', 'strawberry', 'guava', 'pineapple', 'mandarin orange']
+fruits_with_only_two_vowels = [fruit for fruit in fruits if (
+    fruit.count("a") + 
+    fruit.count("e") + 
+    fruit.count("i") + 
+    fruit.count("o") + 
+     fruit.count("u"))  is 2]
+print(fruits_with_only_two_vowels)
 
 # Exercise 5 - make a list that contains each fruit with more than 5 characters
-
+fruits = ['mango', 'kiwi', 'strawberry', 'guava', 'pineapple', 'mandarin orange']
+more_than_five_letter_fruits = [fruit for fruit in fruits if len(fruit) == 5]
+print(five_word_fruits)
 # Exercise 6 - make a list that contains each fruit with exactly 5 characters
-
+fruits = ['mango', 'kiwi', 'strawberry', 'guava', 'pineapple', 'mandarin orange']
+five_letter_fruits = [fruit for fruit in fruits if len(fruit) == 5]
+print(five_letter_fruits)
 # Exercise 7 - Make a list that contains fruits that have less than 5 characters
-
+fruits = ['mango', 'kiwi', 'strawberry', 'guava', 'pineapple', 'mandarin orange']
+less_than_five_letter_fruits = [fruit for fruit in fruits if len(fruit) < 5]
+print(less_than_five_letter_fruits)
 
 # Exercise 8 - Make a list containing the number of characters in each fruit. Output would be [5, 4, 10, etc... ]
-
+fruits = ['mango', 'kiwi', 'strawberry', 'guava', 'pineapple', 'mandarin orange']
+number_of_characters = [len(fruit) for fruit in fruits]
+print(number_of_characters)
 # Exercise 9 - Make a variable named fruits_with_letter_a that contains a list of only the fruits that contain the letter "a"
 
 # Exercise 10 - Make a variable named even_numbers that holds only the even numbers 
