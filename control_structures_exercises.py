c1c8181045b509b7f271b149b6f84d721f3bb901
“Brandon <brandon.j.bryant1@gmail.com>
2021-01-13 10:58:32 -0600

    Added weekend solution

--- control_structures_exercises.py
+++ control_structures_exercises.py
@@ -1,2 +1,27 @@
 #prompt the user for a day of the week
-(What day of the week is it today?)
+today = input("What day of the week is it today? ")
+if today == "Monday" or "monday" :
+        print("Today is Monday my friend! ")
+else :
+        print("Today is not Monday my friend! ")
+    
+    
+
+#prompt the user for a day of the week, print out whether the day is a weekday or a weekend
+today = input("What day of the week is it today? ")
+today = today.lower()
+
+if today == "saturday" or today == "sunday"
+    print(f"{today.capitalize()} is a weekend kinda day!! ")
+else:
+    print(f"{today.capitalize()} is just a weekday, sorry pal. ")
+
+#create variables and make up values for
+            #the number of hours worked in one week 
+            hours_worked = 
+            #the hourly rate
+            hourly_pay = 
+            #how much the week's paycheck will be
+            paycheck_amount = 
+            #write the python code that calculates the weekly paycheck. You get
+            #paid time and a half if you work more than 40 hours
