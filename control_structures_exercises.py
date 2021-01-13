55198b3f96daee02ce3ba7e8b0d0d5ebfb7a0909
“Brandon <brandon.j.bryant1@gmail.com>
2021-01-13 11:42:00 -0600

    Added while loop counting backwords by 2

--- control_structures_exercises.py
+++ control_structures_exercises.py
@@ -50,3 +50,13 @@ while i <= 15:
     i += 1
 #Each loop iteration, output the current value of i, then increment i by one.
 
+#Create a while loop that will count by 2's starting with 0 and ending at 100. Follow each number with a new line.
+#Alter your loop to count backwards by 5's from 100 to -10.
+#Create a while loop that starts at 2, and displays the number squared on each line while the number is less than 1,000,000. Output should equal:
+i = 0
+while i <= 100:
+    print(i)
+    i += 2
+
+
+
