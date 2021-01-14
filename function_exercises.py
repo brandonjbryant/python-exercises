1ab0b5aaab94512ff8aa11ba7f7d4b6833759a1e
“Brandon <brandon.j.bryant1@gmail.com>
2021-01-14 15:59:01 -0600

    Added prompt and began first exercise

---
+++ function_exercises.py
@@ -0,0 +1,30 @@
+# 1. Define a function named is_two. It should accept one input and return True if the passed input is either the number or the string 2, False otherwise.
+
+
+# 2. Define a function named is_vowel. It should return True if the passed string is a vowel, False otherwise.
+
+# 3. Define a function named is_consonant. It should return True if the passed string is a consonant, False otherwise. Use your is_vowel function to accomplish this.
+# 4. Define a function that accepts a string that is a word. The function should capitalize the first letter of the word if the word starts with a consonant.
+
+# 5. Define a function named calculate_tip. It should accept a tip percentage (a number between 0 and 1) and the bill total, and return the amount to tip.
+
+# 6. Define a function named apply_discount. It should accept a original price, and a discount percentage, and return the price after the discount is applied.
+
+# 7. Define a function named handle_commas. It should accept a string that is a number that contains commas in it as input, and return a number as output.
+# 8. Define a function named get_letter_grade. It should accept a number and return the letter grade associated with that number (A-F).
+
+# 9. Define a function named remove_vowels that accepts a string and returns a string with all the vowels removed.
+
+# 10. Define a function named normalize_name. It should accept a string and return a valid python identifier, that is:
+ #A. anything that is not a valid python identifier should be removed
+ #B. leading and trailing whitespace should be removed
+ #C. everything should be lowercase
+ #D. spaces should be replaced with underscores
+    """for example:
+Name will become name
+First Name will become first_name
+% Completed will become completed """
+
+#Write a function named cumulative_sum that accepts a list of numbers and returns a list that is the cumulative sum of the numbers in the list.
+ """cumulative_sum([1, 1, 1]) returns [1, 2, 3]
+    cumulative_sum([1, 2, 3, 4]) returns [1, 3, 6, 10]"""
