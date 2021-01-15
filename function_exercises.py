b2b08ca568513843a11567e9b739ff0e87ff99ad
“Brandon <brandon.j.bryant1@gmail.com>
2021-01-15 09:29:52 -0600

    Added consonant function

--- function_exercises.py
+++ function_exercises.py
@@ -1,9 +1,20 @@
 # 1. Define a function named is_two. It should accept one input and return True if the passed input is either the number or the string 2, False otherwise.
+def is_two(x):
+    return  x == '2' or x == 2
+
 
 
 # 2. Define a function named is_vowel. It should return True if the passed string is a vowel, False otherwise.
+def is_vowel(letter):
+    return letter == 'a' or letter == 'e' or letter == 'i' or letter == 'o' or letter == 'u'
 
-# 3. Define a function named is_consonant. It should return True if the passed string is a consonant, False otherwise. Use your is_vowel function to accomplish this.
+
+# 3. Define a function named is_consonant. It should return True if the passed string is a consonant, False otherwise. Use your is_vowel function to accomplish this. 
+def is_consonant(letter):
+    letters = 'abcdefghijklmnopqrstuvwxyz'
+    return len(letter) == 1 and letter.isalpha() and not is_vowel(letter)
+is_consonant('b')
+
 # 4. Define a function that accepts a string that is a word. The function should capitalize the first letter of the word if the word starts with a consonant.
 
 # 5. Define a function named calculate_tip. It should accept a tip percentage (a number between 0 and 1) and the bill total, and return the amount to tip.
@@ -11,7 +22,9 @@
 # 6. Define a function named apply_discount. It should accept a original price, and a discount percentage, and return the price after the discount is applied.
 
 # 7. Define a function named handle_commas. It should accept a string that is a number that contains commas in it as input, and return a number as output.
+
 # 8. Define a function named get_letter_grade. It should accept a number and return the letter grade associated with that number (A-F).
+
 
 # 9. Define a function named remove_vowels that accepts a string and returns a string with all the vowels removed.
 
@@ -20,11 +33,11 @@
  #B. leading and trailing whitespace should be removed
  #C. everything should be lowercase
  #D. spaces should be replaced with underscores
-    """for example:
-Name will become name
-First Name will become first_name
-% Completed will become completed """
+    ##for example:
+#Name will become name
+#First Name will become first_name
+#% Completed will become completed 
 
 #Write a function named cumulative_sum that accepts a list of numbers and returns a list that is the cumulative sum of the numbers in the list.
- """cumulative_sum([1, 1, 1]) returns [1, 2, 3]
-    cumulative_sum([1, 2, 3, 4]) returns [1, 3, 6, 10]"""
+ #cumulative_sum([1, 1, 1]) returns [1, 2, 3]
+    #cumulative_sum([1, 2, 3, 4]) returns [1, 3, 6, 10]
