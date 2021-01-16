9b3168dab39fb738fba9738f11aa7d45a05b86f5
“Brandon <brandon.j.bryant1@gmail.com>
2021-01-16 13:35:52 -0600

    Import prompt

--- import_exercises.py
+++ import_exercises.py
@@ -1,6 +1,12 @@
 #import and test 3 functions
 #First import
-import function_exercises;
+import function_exercises as f
+f.calculate_tip(.5,300)
+print("You should tip more")                                                                                                                                                               
 
 #second import
-from function_exercises import 
+import function_exercises as funk 
+from function_exercises import apply_discount as discount 
+
+#third import
+#                 
